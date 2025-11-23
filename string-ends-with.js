3a4a64695cee3424e3bee95308d59a366fdd10d9
irinali_is <HellyBad21@gmail.com>
2025-11-23 19:24:15 +0700

    added file string-ends-with.js

---
+++ string-ends-with.js
@@ -0,0 +1,18 @@
+//DESCRIPTION
+
+// Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
+
+// Examples:
+
+// Inputs: "abc", "bc"
+// Output: true
+
+// Inputs: "abc", "d"
+// Output: false
+
+//SOLUTION
+
+function solution(str, ending){
+    return str.endsWith(ending);
+};
+console.log(solution());
