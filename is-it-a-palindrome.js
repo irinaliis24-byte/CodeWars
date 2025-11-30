b579a49c64420a03ede97483c1f9f63f71f3998c
irinali_is <HellyBad21@gmail.com>
2025-11-30 11:35:31 +0700

    added 2 new files

---
+++ is-it-a-palindrome.js
@@ -0,0 +1,13 @@
+// DESCRIPTION:
+// Write a function that checks if a given string (case insensitive) is a palindrome.
+
+// A palindrome is a word, number, phrase, or other sequence of symbols that reads the same backwards as forwards, such as madam or racecar.
+
+//SOLUTION:
+
+function isPalindrome(x) {
+    let str = x.toLowerCase();
+    return str.split('').reverse().join('') === str;
+}
+
+console.log(isPalindrome("madam"));
