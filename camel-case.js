3b4f936d514d7969bfe3a825bf7bb10f9026f6a7
irinali_is <HellyBad21@gmail.com>
2025-12-05 20:09:53 +0700

    added new file:   camel-case.js

---
+++ camel-case.js
@@ -0,0 +1,17 @@
+// DESCRIPTION:
+// Write a method (or function, depending on the language) that converts a string to camelCase, that is, all words must have their first letter capitalized and spaces must be removed.
+
+// Examples (input --> output):
+// "hello case" --> "HelloCase"
+// "camel case word" --> "CamelCaseWord"
+
+//SOLUTION:
+
+String.prototype.camelCase = function () {
+    return this
+        .split(" ")
+        .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+        .join("");
+};
+
+console.log("camel Case method".camelCase())
