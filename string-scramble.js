8b4b8e33ffe424cd653c0ea176ae3007be6cb706
irinali_is <HellyBad21@gmail.com>
2025-12-12 16:49:35 +0700

    added string-scramble.js

---
+++ string-scramble.js
@@ -0,0 +1,32 @@
+//DESCRIPTION:
+// Given a string and an array of indices, rearrange the characters of the string so that each character is placed at the position specified by the corresponding index in the array.
+
+// Example
+// input: "abcd", [0, 3, 1, 2]
+// output: "acdb"
+// Explanation
+// The character 'a' is placed at index 0.
+
+// The character 'b' is placed at index 3.
+
+// The character 'c' is placed at index 1.
+
+// The character 'd' is placed at index 2.
+
+// Notes
+// The string and the array will be of equal length.
+
+// The string will contain valid characters (A-Z, a-z, or 0-9);
+// the array will contain valid indices.
+
+//SOLUTION:
+
+function scramble(str, arr) {
+    const result = [];
+    for (let i = 0; i < str.length; i++) {
+        result[arr[i]] = str[i];
+    }
+    return result.join("");
+};
+
+console.log(scramble("abcd", [0, 3, 1, 2]))
